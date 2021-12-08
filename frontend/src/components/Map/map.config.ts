18d94216f91777476a1fd653a3b35e41cd03fedd
philip <philipatdev@gmail.com>
2021-12-08 20:56:10 +0200

    Project setup

---
+++ frontend/src/components/Map/map.config.ts
@@ -0,0 +1,9 @@
+const mapConfig = {
+    center: {
+        lat: 49.54,
+        lng: 24.19
+    },
+    zoom: 5
+} as google.maps.MapOptions
+
+export { mapConfig };
---
+++ frontend/src/mocked_data/capitals-ltd-lng.ts
@@ -0,0 +1,53 @@
+export const capitalsLtdLng = [
+    {
+        capital: 'Kiev',
+        ltd: '50.4500',
+        lng: '30.5233'
+    },
+    {
+        capital: 'Warsaw',
+        ltd: '52.2370',
+        lng: '21.0175'
+    },
+    {
+        capital: 'Prague',
+        ltd: '50.0736',
+        lng: '14.4185'
+    },
+    {
+        capital: 'Paris',
+        ltd: '48.8647',
+        lng: '2.3490'
+    },
+    {
+        capital: 'Berlin',
+        ltd: '52.5200',
+        lng: '13.4049'
+    },
+    {
+        capital: 'London',
+        ltd: '51.5098',
+        lng: '-0.1180'
+    },
+    {
+        capital: 'Madrid',
+        ltd: '40.4167',
+        lng: '-3.7037'
+    },
+    {
+        capital: 'Rome',
+        ltd: '41.9027',
+        lng: '12.4963'
+    },
+    {
+        capital: 'Washington',
+        ltd: '38.8892',
+        lng: '-77.0506'
+    },
+    {
+        capital: 'Canberra',
+        ltd: '-35.2820',
+        lng: '149.1289'
+    }
+]
+
